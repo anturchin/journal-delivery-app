0cd4a429021d68c58a72eb0f45e35dffccf5a92f
anturchin <tur4intwo@gmail.com>
2024-01-06 12:19:43 +0300

    feat: add style css in journal-form

--- journal/src/components/JournalForm/JournalForm.jsx
+++ journal/src/components/JournalForm/JournalForm.jsx
@@ -50,8 +50,8 @@ const JournalForm = ({updateList}) => {
 
 	const styleTitle = 
 	formValid.title 
-		? styles['journal-form__input'] 
-		: cn(styles['journal-form__input'], styles['invalid']);
+		? styles['journal-form__title'] 
+		: cn(styles['journal-form__title'], styles['invalid']);
 	const styleDate = 
 	formValid.date 
 		? styles['journal-form__input'] 
@@ -63,21 +63,41 @@ const JournalForm = ({updateList}) => {
 
 	return (
 		<form className={styles['journal-form']} onSubmit={onSubmitForm}>
-			<input 
-				className={styleTitle}  
-				type="text" 
-				name="title" 
-			/>
-			<input 
-				className={styleDate}
-				type="date" 
-				name="date"
-			/>
-			<input 
-				className={styles['journal-form__input']} 
-				type="text" 
-				name="tag" 
-			/>
+			<div>
+				<input 
+					className={styleTitle}  
+					type="text" 
+					name="title" 
+				/>
+			</div>
+			<div className={styles['journal-form__row']}>
+				<label 
+					className={styles['journal-form__label']} 
+					htmlFor="date">
+					<img src="/calendar.svg" alt="caleтdar" />
+					<span>Дата</span>
+				</label>
+				<input 
+					className={styleDate}
+					type="date" 
+					name="date"
+					id="date"
+				/>
+			</div>
+			<div className={styles['journal-form__row']}>
+				<label 
+					className={styles['journal-form__label']} 
+					htmlFor="tag">
+					<img src="/folder.svg" alt="folder" />
+					<span>Метки</span>
+				</label>
+				<input 
+					className={styles['journal-form__input']} 
+					type="text" 
+					name="tag" 
+					id="tag"
+				/>
+			</div>
 			<textarea 
 				className={styleText} 
 				name="text"
